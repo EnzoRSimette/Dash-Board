e6746500f40cb97e5e4c986bfd6d35c10f06dc81
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-01-08 17:48:23 -0300

    Consegui fazer ele enviar um xlsx

---
+++ drag&drop/script.js
@@ -0,0 +1,6 @@
+const upload = document.querySelector(".espaco");
+const inputinvisivel = document.querySelector("#arquivo");
+function abrirseletor() {
+    inputinvisivel.click();
+}
+upload.addEventListener("click", abrirseletor);
