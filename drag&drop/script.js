307b1990b4bc4aada6a31b535332da5f752b595c
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-01-09 09:12:22 -0300

    Adicionei drag&drop

--- drag&drop/script.js
+++ drag&drop/script.js
@@ -1,10 +1,15 @@
 const upload = document.querySelector(".espaco");
 const arquivo_importado = document.querySelector("#arquivo_importado");
 const inputinvisivel = document.querySelector("#arquivo");
+upload.addEventListener("dragover", aoarrastarsobre);
+upload.addEventListener("drop", aosoltar);
+
 function abrirseletor() {
     inputinvisivel.click();
 }
+
 upload.addEventListener("click", abrirseletor);
+
 function processararquivo() {
     const dados = inputinvisivel.files[0];
     arquivo_importado.textContent = `${dados.name}`;
@@ -21,4 +26,17 @@ function processararquivo() {
     }
     console.log(dados);
 }
+
+function aoarrastarsobre(event) {
+    event.preventDefault();
+    console.log("O arquivo está pairando sobre a div");
+}
+
+function aosoltar(event) {
+    event.preventDefault();
+    const arquivosSoltos = event.dataTransfer.files[0];
+    arquivo_importado.textContent = `${arquivosSoltos.name}`;
+    console.log("Você soltou o arquivo:", arquivosSoltos.name);
+}
+
 inputinvisivel.addEventListener("change", processararquivo);
