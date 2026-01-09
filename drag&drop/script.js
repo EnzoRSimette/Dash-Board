91cf9f85dd38e0a840578af3c268f267addc277e
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-01-09 08:42:40 -0300

    Consegui adicionar algumas validações

--- drag&drop/script.js
+++ drag&drop/script.js
@@ -1,6 +1,24 @@
 const upload = document.querySelector(".espaco");
+const arquivo_importado = document.querySelector("#arquivo_importado");
 const inputinvisivel = document.querySelector("#arquivo");
 function abrirseletor() {
     inputinvisivel.click();
 }
 upload.addEventListener("click", abrirseletor);
+function processararquivo() {
+    const dados = inputinvisivel.files[0];
+    arquivo_importado.textContent = `${dados.name}`;
+    if (dados) {
+        console.log("Arquivo existe");
+    } else {
+        console.log("Arquivo não existe");
+    }
+    if (dados.size >= 524288000) {
+        alert("Arquivo é muito grande!");
+        console.clear();
+    } else {
+        console.log("Arquivo aceito!");
+    }
+    console.log(dados);
+}
+inputinvisivel.addEventListener("change", processararquivo);
