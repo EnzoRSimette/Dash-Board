eb0a8c4849aa3cc8303e189423c331b283416993
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-01-12 15:19:47 -0300

    Consegui corrigir os erros no código sozinho

--- drag&drop/script.js
+++ drag&drop/script.js
@@ -1,9 +1,8 @@
 const upload = document.querySelector(".espaco");
 const imagem = upload.querySelector(".imagem");
-const arquivo_importado = document.querySelector("#arquivo_importado");
+const arquivo_importado = document.querySelector("#arquivo");
+const arquivo_importado1 = document.querySelector("#arquivo_importado");
 const inputinvisivel = document.querySelector("#arquivo");
-upload.addEventListener("dragover", aoarrastarsobre);
-upload.addEventListener("drop", aosoltar);
 
 function abrirseletor() {
     inputinvisivel.click();
@@ -11,18 +10,17 @@ function abrirseletor() {
 
 upload.addEventListener("click", abrirseletor);
 
-function validarEExibir () {
+function validarEExibir() {
     if (!arquivo_importado.name.endsWith(".xlsx")) {
         alert("Coloque apenas arquivos xlsx, por favor");
         return;
     }
-    arquivo_importado.textContent = `${dados.name}`;
     if (dados) {
         console.log("Arquivo existe");
     } else {
         console.log("Arquivo não existe");
     }
-    if (dados.size >= 524288000) {
+    if (dados.size >= 5242880) {
         alert("Arquivo é muito grande!");
         console.clear();
     } else {
@@ -49,11 +47,35 @@ upload.addEventListener("drop", () => {
     imagem.classList.remove("dragging");
 });
 
-function aosoltar(event) {
+function validarEExibir_dragndrop(event) {
     event.preventDefault();
-    const arquivosSoltos = validarEExibir(event.dataTransfer.files[0]);
-    arquivo_importado.textContent = `${arquivosSoltos.name}`;
+    const arquivosSoltos = event.dataTransfer.files[0];
+    if (!arquivosSoltos.name.endsWith(".xlsx")) {
+        alert("Coloque apenas arquivos xlsx, por favor");
+        return;
+    }
+    if (arquivosSoltos) {
+        console.log("Arquivo existe");
+    } else {
+        console.log("Arquivo não existe");
+    }
+    if (arquivosSoltos.size >= 5242880) {
+        alert("Arquivo é muito grande!");
+        console.clear();
+    } else {
+        console.log("Arquivo aceito!");
+    }
+    console.log(arquivosSoltos);
     console.log("Você soltou o arquivo:", arquivosSoltos.name);
 }
 
-validarEExibir.addEventListener("change", processararquivo);
+
+function aosoltar(event) {
+
+
+    validarEExibir_dragndrop(arquivosSoltos);
+
+}
+
+upload.addEventListener("dragover", aoarrastarsobre);
+upload.addEventListener("drop", validarEExibir_dragndrop);
