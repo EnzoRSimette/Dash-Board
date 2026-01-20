c4aeb6465185a44d0d804f52845104e86b9b67b5
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-01-20 13:26:41 -0300

    DEU CERTO

--- drag&drop/php/processador.php
+++ drag&drop/php/processador.php
@@ -1,6 +1,11 @@
 <?php
 $arquivo = fopen($_FILES["arquivo"]["tmp_name"], 'r');
 $cabeçalho = mb_convert_encoding(fgetcsv($arquivo, null, ';'), 'UTF-8');
+function limpar_texto($input) {
+    $input = mb_convert_encoding($input, 'UTF-8');
+    $input = str_replace(',','.', $input);
+    return $input;
+};
 $dados_finais = [
     'codigo_orgao_superior' => [],
     'nome_orgao_superior' => [],
@@ -19,29 +24,25 @@ $dados_finais = [
 ];
 while ($informacoes = fgetcsv($arquivo, null, ';')) {
     if ($informacoes !== false || null) {
-        foreach ($informacoes as $i) {
-            $informacoes[$i] = mb_convert_encoding($informacoes[$i], 'UTF-8');
-            $informacoes[$i] = str_replace($i, ',', '.');
-        };
-        $dados_finais['codigo_orgao_superior'][] = $informacoes[0];
-        $dados_finais['nome_orgao_superior'][] = $informacoes[1];
-        $dados_finais['codigo_orgao'][] = $informacoes[2];
-        $dados_finais['nome_unidade_gestora'][] = $informacoes[3];
-        $dados_finais['categoria_economica'][] = $informacoes[4];
-        $dados_finais['origem_receita'][] = $informacoes[5];
-        $dados_finais['especie_receita'][] = $informacoes[6];
-        $dados_finais['detalhamento'][] = $informacoes[7];
-        $dados_finais['valor_previsto'][] = $informacoes[8];
-        $dados_finais['valor_lancado'][] = $informacoes[9];
-        $dados_finais['valor_realizado'][] = $informacoes[10];
-        $dados_finais['percentual_realizado'][] = $informacoes[11];
-        $dados_finais['data_lancada'][] = $informacoes[12];
-        $dados_finais['ano_exercicio'][] = $informacoes[13];
+        $dados_finais['codigo_orgao_superior'][] = limpar_texto($informacoes[0]);
+        $dados_finais['nome_orgao_superior'][] = limpar_texto($informacoes[1]);
+        $dados_finais['codigo_orgao'][] = limpar_texto($informacoes[2]);
+        $dados_finais['nome_unidade_gestora'][] = limpar_texto($informacoes[3]);
+        $dados_finais['categoria_economica'][] = limpar_texto($informacoes[4]);
+        $dados_finais['origem_receita'][] = limpar_texto($informacoes[5]);
+        $dados_finais['especie_receita'][] = limpar_texto($informacoes[6]);
+        $dados_finais['detalhamento'][] = limpar_texto($informacoes[7]);
+        $dados_finais['valor_previsto'][] = limpar_texto($informacoes[8]);
+        $dados_finais['valor_lancado'][] = limpar_texto($informacoes[9]);
+        $dados_finais['valor_realizado'][] = limpar_texto($informacoes[10]);
+        $dados_finais['percentual_realizado'][] = limpar_texto($informacoes[11]);
+        $dados_finais['data_lancada'][] = limpar_texto($informacoes[12]);
+        $dados_finais['ano_exercicio'][] = limpar_texto($informacoes[13]);
     }
 }
-$quantidades = [];
-foreach ($dados_finais as $i) {
-        $quantidades[] = array_count_values($i);
-}
-json_encode($dados_finais);
+// $quantidades = [];
+// foreach ($dados_finais as $i) {
+//     $quantidades[] = array_count_values($i);
+// }
+echo json_encode($dados_finais);
 fclose($arquivo);
