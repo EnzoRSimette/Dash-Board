5c90051a8e3de194e1c8d7e42c155ead93cccc88
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-01-27 11:26:01 -0300

    Mudei o charset para latin1

--- drag&drop/php/processador.php
+++ drag&drop/php/processador.php
@@ -2,13 +2,14 @@
 $caminho = $_FILES["arquivo"]["tmp_name"];
 $caminho = str_replace('\\', '/', $caminho);
 
-$ligacao = new PDO("mysql:host=localhost;dbname=dashboard;charset=utf8mb4", "root", "", [PDO::MYSQL_ATTR_LOCAL_INFILE => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Pode usar a mesma estrutura sempre
+$ligacao = new PDO("mysql:host=localhost;dbname=dashboard;charset=latin1", "root", "", [PDO::MYSQL_ATTR_LOCAL_INFILE => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Pode usar a mesma estrutura sempre
 
 $cols = "`CODIGO_ORGAO_SUPERIOR`, `NOME_ORGAO_SUPERIOR`, `CODIGO_ORGAO`, `NOME_ORGAO`, `CODIGO_UNIDADE_GESTORA`, `NOME_UNIDADE_GESTORA`, `CATEGORIA_ECONOMICA`, `ORIGEM_RECEITA`,
 `ESPECIE_RECEITA`, `DETALHAMENTO`, `VALOR_PREVISTO_ATUALIZADO`, `VALOR_LANCADO`, `VALOR_REALIZADO`, `PERCENTUAL_REALIZADO`, `DATA_LANCAMENTO`, ANO_EXERCICIO";
 
 
-$sql_query = "LOAD DATA LOCAL INFILE '$caminho' IGNORE INTO TABLE `dados` CHARACTER SET utf8mb4 FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES (@ColVar0, `NOME_ORGAO_SUPERIOR`, @ColVar2, `NOME_ORGAO`, @ColVar4, `NOME_UNIDADE_GESTORA`, `CATEGORIA_ECONOMICA`, `ORIGEM_RECEITA`, `ESPECIE_RECEITA`, `DETALHAMENTO`, @ColVar10, @ColVar11, @ColVar12, @ColVar13, `DATA_LANCAMENTO`, @ColVar15)
+$sql_query = "
+LOAD DATA LOCAL INFILE '$caminho' IGNORE INTO TABLE `dados` CHARACTER SET latin1 FIELDS TERMINATED BY ';' OPTIONALLY ENCLOSED BY '\"' ESCAPED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES (@ColVar0, `NOME_ORGAO_SUPERIOR`, @ColVar2, `NOME_ORGAO`, @ColVar4, `NOME_UNIDADE_GESTORA`, `CATEGORIA_ECONOMICA`, `ORIGEM_RECEITA`, `ESPECIE_RECEITA`, `DETALHAMENTO`, @ColVar10, @ColVar11, @ColVar12, @ColVar13, `DATA_LANCAMENTO`, @ColVar15)
 SET
 `CODIGO_ORGAO_SUPERIOR` = REPLACE(REPLACE(@ColVar0, '.', ''), ',', '.'),
 `CODIGO_ORGAO` = REPLACE(REPLACE(@ColVar2, '.', ''), ',', '.'),
@@ -19,4 +20,12 @@ SET
 `PERCENTUAL_REALIZADO` = REPLACE(REPLACE(@ColVar13, '.', ''), ',', '.'),
 `ANO_EXERCICIO` = REPLACE(REPLACE(@ColVar15, '.', ''), ',', '.');";
 
-try {$ligacao->query($sql_query)->fetchAll(); echo "Dados importados";} catch (Exception $ex) { echo $ex->getMessage(); }
+$limpar_dados = "
+TRUNCATE TABLE `dados`
+";
+
+try {
+    $ligacao->exec($limpar_dados); echo "Dados Limpos" . PHP_EOL;
+} catch (Exception $ex) { echo $ex->getMessage(); }
+
+try {$ligacao->query($sql_query)->fetchAll(); echo "Dados importados" . PHP_EOL;} catch (Exception $ex) { echo $ex->getMessage(); }
