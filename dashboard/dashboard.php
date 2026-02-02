b53ade0e451d96c8fe39d193096c86917c2427c5
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-02-02 16:48:32 -0300

    eh

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -11,15 +11,20 @@
 
 <body>
     <div class="bg-zinc-100 min-h-screen min-w-screen"> <!-- dark:bg-slate-800 -->
-        <div id="links" class="min-w-screen bg-white h-10 border-b-2 border-sky-300">
-        </div>
+        <div id="links" class="min-w-screen bg-white h-10 border-b-2 border-sky-300"></div>
         <div class="grid-cols-3 gap-x-3 p-2 grid-flow-col flex h-75 grow">
             <div id="grafico_media_no" class="bg-white rounded-md shadow-md flex-1"></div>
             <div id="grafico_mdia_nos" class="bg-white rounded-md shadow-md flex-1"></div>
             <div id="grafico_ma_outro" class="bg-white rounded-md shadow-md flex-1"></div>
         </div>
+        <div class="grid-cols-2 gap-x-3 p-2 grid-flow-col flex h-143 grow">
+            <div id="grafico_porcentagem_nos" class="bg-white rounded-md shadow-md flex-1"></div>
+            <div id="grafico2" class="bg-white rounded-md shadow-md flex-1"></div>
+        </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> -->
+    <script src="C:\laragon\www\node_modules\chart.js\dist\chart.umd.min.js"></script>
+    <script src="C:\laragon\www\node_modules\chartjs-chart-treemap\dist\chartjs-chart-treemap.min.js"></script>
     <script src="dashboard.js"></script>
 </body>
 
